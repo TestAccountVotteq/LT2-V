ebf12a492a7df80e9c61dc3ebe8a587b6e683891
Votteq <104949955+TestAccountVotteq@users.noreply.github.com>
2024-07-07 20:54:59 -0400

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -1,1 +1,15 @@
-print("Script loaded")
+local RunService = game:GetService("RunService")
+local gamePlayers = game:GetService("Players")
+local localUser = gamePlayers.LocalPlayer
+local localCharacter = localUser.Character
+
+local playerValues = {
+    walkSpeed = 16
+    jumpPwr = 60
+    flySpeed = 80
+}
+
+RunService.Heartbeat:Connect(function()
+	localCharacter:FindFirstChildWhichIsA("Humanoid").WalkSpeed = playerValues.walkSpeed
+    localCharacter:FindFirstChildWhichIsA("Humanoid").JumpPower = playerValues.jumpPwr
+end)
